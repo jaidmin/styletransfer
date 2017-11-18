a84e43e7d8de1a8c3fe45cb836c3eb570093ce51
Magnus <magnus.kleinau@law-school.de>
2017-11-18 20:07:22 +0000

    Added: ImageHandling
    ImageHandling module added to read images from files and convert them to [[[Float]]] and convert [[[Float]]] into images and save them.
    Dependencies changed:  testJuicyPixels
                         , JuicyPixels >= 3.2.9.1
                         , filepath >= 1.4.1.2
                         , bytestring >= 0.10.8.1
                         , vector >= 0.12.0.1

---
+++ src/ImageHandling.hs
@@ -0,0 +1,49 @@
+module ImageHandling
+    ( listFromImage, imageFromList
+    ) where
+
+import Codec.Picture
+import System.FilePath (replaceExtension)
+import qualified Codec.Picture.Types as M
+import qualified Data.Vector.Storable as V
+import Data.Word
+import Data.List
+
+cleanImg :: Either String DynamicImage -> Image PixelRGB8
+cleanImg (Left err) =  error "Could not read Image"
+cleanImg (Right img)  =  convertRGB8 img
+   
+type Path = String
+
+channels :: Int
+channels = 3
+
+listFromImage :: Path -> IO [[[Float]]]
+listFromImage path = do
+  eimg <- readImage path
+  let img = cleanImg eimg
+  let list = case img of (Image _ _ imageData) ->  V.toList imageData
+  let width = case img of (Image width _ _) ->  width
+  print width
+  let height = case img of (Image _ height _) ->  height
+  print height
+  let newList = ((splitEvery height) . (splitEvery channels) . convertWF) list 
+  return newList
+
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery _ [] = []
+splitEvery x ls = take x ls : splitEvery x (drop x ls)
+
+convertWF :: [Word8] -> [Float]
+convertWF ws = [ (fromIntegral w) :: Float | w <- ws]
+
+convertFW :: [Float] -> [Word8]
+convertFW fs = [ (round f) :: Word8 | f <- fs]
+
+imageFromList :: [[[Float]]] -> Path -> IO ()
+imageFromList list path = do 
+  let width = length list
+  let height = length $ head list
+  let newList = (convertFW . concat . concat) list
+  let image = ImageRGB8 (Image width height (V.fromList newList))
+  savePngImage path image
